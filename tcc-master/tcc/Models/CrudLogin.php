29444e6540d5ed10f310a7177af3f46969dc7ea9
Pedro Henrique <pedroca0312@gmail.com>
2018-08-07 22:45:39 -0300

    Add files via upload

---
+++ tcc-master/tcc/Models/CrudLogin.php
@@ -0,0 +1,46 @@
+<?php
+
+require_once 'BDConection.php';
+require_once 'Usuario.php';
+
+class CrudLogin
+{
+    public $conexao;
+
+    public function GetUsuarios()
+    {
+        $this->conexao = BDConection::getConexao();
+
+        $sql = "select * from usuario";
+
+        $res = $this->conexao->query($sql);
+
+        $usu = [];
+        
+        $usuarios = $res->fetchAll(PDO::FETCH_ASSOC);
+        foreach ($usuarios as $usuario){
+            $usu[] = new Usuario($usuario['nome'], $usuario['email'], $usuario['senha'],$usuario['id_usuario'], $usuario['username']);
+        }
+        return $usu;
+
+
+    }
+
+    public function CadastrarUsuario(Usuario $usuario){
+        $this->conexao = BDConection::getConexao();
+
+        $usu[] = $usuario->getNome();
+        $usu[] = $usuario->getEmail();
+        $usu[] = $usuario->getSenha();
+        $usu[] = $usuario->getUsername();
+
+
+
+        $sql = "insert into usuario (nome,email,senha,username) values ('".$usu[0]."','".$usu[1]."','".$usu[2]."','".$usu[3]."')";
+
+        $res = $this->conexao->query($sql);
+
+    }
+
+
+}
---
+++ tcc-master/tcc/controlador/usuario.php
@@ -0,0 +1,39 @@
+<?php
+require_once '../Models/CrudLogin.php';
+require_once '../Models/Usuario.php';
+
+if (isset($_GET['acao'])){
+    $acao = $_GET['acao'];
+}else{
+    $acao = 'verifica';
+}
+
+switch ($acao) {
+    case 'verifica':
+
+        $crud = new CrudLogin();
+        if (isset($_POST['gravar'])) {
+            $usuarios = $crud->GetUsuarios();
+            foreach ($usuarios as $usuario) {
+                if ($_POST['usuario'] == $usuario->username and $_POST['senha'] == $usuario->senha) {
+                    header('Location: ../fullcalendar-3.9.0/index.php');
+                }
+            }
+        } else {
+            include '../views/login.php';
+
+        }
+        break;
+    case 'cadastrar':
+        $crud = new CrudLogin();
+
+        if (isset($_GET)){
+            include '../views/cadastro.php';
+        }
+        if (isset($_POST['botao'])){
+            $usuario = new Usuario($_POST['nome'],$_POST['email'],$_POST['senha'],Null,$_POST['usuario']);
+        $crud->CadastrarUsuario($usuario);
+
+        header('Location: usuario.php');
+        }
+}
