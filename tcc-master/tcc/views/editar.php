8759a3120697456b7959b4ee3b0aa871ae826b8d
Pedro Henrique <pedroca0312@gmail.com>
2018-08-30 09:17:09 -0300

    Add files via upload

--- tcc-master/tcc/views/editar.php
+++ tcc-master/tcc/views/editar.php
@@ -1,11 +1,19 @@
 <html>
-    <body>
-    <form method="post" action="">
+<head>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css" integrity="sha384-9gVQ4dYFwwWSjIDZnLEWnxCjeSWFphJiwGPXr1jddIhOegiu1FwO5qRGvFXOdJZ4" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="../bootstrap-4.1.0-dist/css/bootstrap-grid.min.css">
+    <script type="text/javascript" src="../bootstrap-4.1.0-dist/js/bootstrap.bundle.min.js"></script>
 
-        <input name="descricao" type="text" value="<?= $atividade->getDescricao();?>">
-        <br>
-        <input name="data_previsao" type="text" value="<?= $atividade->getDataPrevisao();?>">
-        <input name="id_atividade" type="hidden" value="<?= $atividade->getIdAtividade();?>">
+    <style type="text/css">@import url(../css/style_editar.css); </style>
+</head>
+    <body>
+    <form method="post" action="" class="formularios">
+
+        <input class="form-control inputLogin" name="descricao" type="text" value="<?= $atividade->getDescricao();?>"><br>
+
+        <input class="form-control inputLogin" name="data_previsao" type="text" value="<?= $atividade->getDataPrevisao();?>">
+
+        <input class="btn btn-primary btn-lg" name="id_atividade" type="hidden" value="<?= $atividade->getIdAtividade();?>"> <br>
 
         <a href="../controlador/atividade.php?acao=editar"><button type="submit" name="edita" class="btn btn-primary btn-lg">Editar</button></a>
 
